74a27a727d957ed54f994090a9e23d7a663de2f6
Prakash <prakashku5566singh@gmail.com>
2022-09-20 12:20:21 +0530

    started

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+
+mongoose.connect("mongodb+srv://user_1:password45@cluster0.d0linvl.mongodb.net/group8Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
