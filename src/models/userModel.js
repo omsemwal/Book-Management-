70f148c79034efa840e703055e9f88fb859043a9
Prakash <prakashku5566singh@gmail.com>
2022-09-20 12:45:40 +0530

    group 09

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
 
-    title: { type:string, required:true, enum:["Mr", "Mrs", "Miss"] },
-    name: { type:string, required:true },
-    phone: { type:string, required:true, unique:true },
-    email: { type:string, required:true,  unique:true },
-    password: { type:string, required:true},
+    title: { type:String, required:true, enum:["Mr", "Mrs", "Miss"] },
+    name: { type:String, required:true },
+    phone: { type:String, required:true, unique:true },
+    email: { type:String, required:true,  unique:true },
+    password: { type:String, required:true},
     address: {
-        street: { type:string },
-        city: { type:string },
-        pincode: { type:string }
+        street: { type:String },
+        city: { type:String },
+        pincode: { type:String }
     }
 },
     { timestamps: true });
