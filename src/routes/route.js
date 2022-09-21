7e49942715246f31a1c53f8d7e1bed8a1c6952eb
Prakash <prakashku5566singh@gmail.com>
2022-09-21 06:17:08 +0530

    updated status code

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const user=require('../controllers/userController')
+//.................test................................. */
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
+//.............Api............................
 
 router.post("/createUser",user.createUser)
 router.post("/login",user.login)
